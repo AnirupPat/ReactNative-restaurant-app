b67607d2710291f61cd4b8865a077e43b0b8434f
Anirup Patnaik <anirup049@gmail.com>
2020-06-16 15:42:56 +0530

    Extracting hook logic

---
+++ src/hooks/useResults.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import yelp from '../api/yelp';
+
+export default () => {
+    const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const searchApi = async (searchTerm) => {
+    console.log('Hi there !')
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 30,
+          term: searchTerm,
+          location: 'San Jose'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch(e) {
+      console.log(e);
+      setErrorMessage('Something went wrong !')
+    }
+  };
+
+  // call searchApi when component is first rendered
+  // this is bad code
+  useEffect(() => {
+    searchApi('Pasta');
+  }, []);
+
+  return [searchApi, results , errorMessage];
+};
