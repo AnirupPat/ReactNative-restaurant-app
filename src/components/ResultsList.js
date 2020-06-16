f7210e5c85a9d83fe133dfd327227b76be110305
Anirup Patnaik <anirup049@gmail.com>
2020-06-16 18:04:51 +0530

    FlatList rendering

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 const ResultsList = ({ title, results }) => {
     return (
         <View>
             <Text style={styles.title}>{title}</Text>
-            <Text>Results: {results.length}</Text>
+            <FlatList
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                data={results}
+                keyExtractor={(result) => result.id}
+                renderItem={( {item} ) => {
+                    return (
+                        <Text>{item.name}</Text>
+                    )
+                }}
+            />    
         </View>
     )
 };
