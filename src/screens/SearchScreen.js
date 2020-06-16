e6b4baf68c5380cd4805e1b93aacf32276eea171
Anirup Patnaik <anirup049@gmail.com>
2020-06-16 15:32:42 +0530

    UseEffect

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -8,12 +8,13 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('')
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
+    console.log('Hi there !')
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 30,
-          term: term,
+          term: searchTerm,
           location: 'San Jose'
         }
       });
@@ -24,15 +25,21 @@ const SearchScreen = () => {
     }
   };
 
+  // call searchApi when component is first rendered
+  // this is bad code
+  useEffect(() => {
+    searchApi('Pasta');
+  }, []);
+
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => searchApi()}
+        onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null} 
-      <Text>We have found {results.length}</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
